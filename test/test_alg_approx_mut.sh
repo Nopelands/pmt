3718ef9774c575952cc2723def882cc9073bb8fe
Nopelands <rhas@cin.ufpe.br>
2022-04-05 15:09:32 -0300

    added partial test results

--- test/test_alg_approx_mut.sh
+++ test/test_alg_approx_mut.sh
@@ -31,17 +31,6 @@ for i in $(seq $sizes); do
     err=$((err+1))
 done
 
-echo "agrep (${count} results per pattern size)" | tee -a $logfile
-pattern="E"
-increment="E"
-err=0
-for i in $(seq $sizes); do
-    for i in $(seq $count); do
-        /usr/bin/time -f %e -o $logfile -a agrep -c -$err $pattern $text
-    done
-    pattern=$pattern$increment
-    err=$((err+1))
-done
 
 sizes=16
 
